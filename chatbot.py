93a2de0b95d7e24f01f6b8ea38267c03a4d30fa1
Ruan Costa <costaruan@outlook.com>
2018-03-14 11:07:53 -0300

    Updating chatbot

--- chatbot.py
+++ chatbot.py
@@ -24,12 +24,29 @@ def clean_text(text):
     return text
 
 
+def encoder_rnn_layer(rnn_inputs, rnn_size, num_layers, keep_prob, sequence_length):
+    lstm = tf.contrib.rnn.BasicLSTMCell(rnn_size)
+    lstm_dropout = tf.contrib.rnn.DropoutWrapper(lstm, input_keep_prob=keep_prob)
+    encoder_cell = tf.contrib.rnn.MultiRNNCell([lstm_dropout] * num_layers)
+    _, encoder_state = tf.nn.bidirectional_dynamic_rnn(cell_fw=encoder_cell, cell_bw=encoder_cell,
+                                                       sequence_length=sequence_length, inputs=rnn_inputs,
+                                                       dtype=tf.float32)
+    return encoder_state
+
+
 def model_inputs():
     inputs = tf.placeholder(dtype=tf.int32, shape=[None, None], name='input')
     targets = tf.placeholder(dtype=tf.int32, shape=[None, None], name='target')
     lr = tf.placeholder(dtype=tf.float32, name='learning_rate')
     keep_prob = tf.placeholder(dtype=tf.float32, name='keep_prob')
     return inputs, targets, lr, keep_prob
+
+
+def preprocess_targets(targets, word2int, batch_size):
+    left_side = tf.fill([batch_size, 1], word2int['<SOS>'])
+    right_side = tf.strided_slice(targets, [0, 0], [batch_size, -1], [1, 1])
+    preprocessed_targets = tf.concat([left_side, right_side], 1)
+    return preprocessed_targets
 
 
 # Importing the dataset
